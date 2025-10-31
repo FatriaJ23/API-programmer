aac1a560586b96eaa2c5570a2cef6eccbdfa53f0
fatria jumara adha <jumaraadha@gmail.com>
2025-10-31 18:22:35 +0700

    update database

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,18 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
+import "dotenv/config";
+
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 8889,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "nutech",
-});
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+}).promise();
 
 db.getConnection()
-    .then(() => console.log("Connected to MySQL Database!"))
-    .catch((err) => console.error("Database connection failed:", err.message));
+    .then(() => console.log("Connected to MySQL (Railway)!"))
+    .catch((err) => console.error("Database connection failed", err.message));
 
 export default db;
+
