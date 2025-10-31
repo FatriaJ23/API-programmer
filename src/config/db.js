f9b23210213a81e59ef18d4fc9adba9b43f1c120
fatria jumara adha <jumaraadha@gmail.com>
2025-10-31 21:57:34 +0700

    update database

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: "caboose.proxy.rlwy.net",
+    port: 35182,
+    user: "root",
+    password: "KQDZPzHkwdHmuKQwoaOXrAFupLzEuSuP",
+    database: "railway",
 }).promise();
 
 db.getConnection()
